c7c24b65f95f9182b55073f0294f132f8ba53f84
bibaijin <bibaijin@gmail.com>
2015-08-29 11:01:26 +0800

    works well

--- net/conn.go
+++ net/conn.go
@@ -10,34 +10,34 @@ type TimeoutConn struct {
 	conn    net.Conn
 }
 
-func NewTimeoutConn(timeout time.Duration, conn net.Conn) *TimeoutConn {
-	tc := &TimeoutConn{
+func NewTimeoutConn(timeout time.Duration, conn net.Conn) TimeoutConn {
+	tc := TimeoutConn{
 		timeout: timeout,
 		conn:    conn,
 	}
 	return tc
 }
 
-func (tc *TimeoutConn) Read(b []byte) (n int, err error) {
+func (tc TimeoutConn) Read(b []byte) (n int, err error) {
 	tc.conn.SetReadDeadline(time.Now().Add(tc.timeout))
 	n, err = tc.conn.Read(b)
 	return n, err
 }
 
-func (tc *TimeoutConn) Write(b []byte) (n int, err error) {
+func (tc TimeoutConn) Write(b []byte) (n int, err error) {
 	tc.conn.SetWriteDeadline(time.Now().Add(tc.timeout))
 	n, err = tc.conn.Write(b)
 	return n, err
 }
 
-func (tc *TimeoutConn) Close() error {
+func (tc TimeoutConn) Close() error {
 	return tc.conn.Close()
 }
 
-func (tc *TimeoutConn) LocalAddr() net.Addr {
+func (tc TimeoutConn) LocalAddr() net.Addr {
 	return tc.conn.LocalAddr()
 }
 
-func (tc *TimeoutConn) RemoteAddr() net.Addr {
+func (tc TimeoutConn) RemoteAddr() net.Addr {
 	return tc.conn.RemoteAddr()
 }
